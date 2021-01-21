f22f62f1e47cf4eec8d270d6098a12610ef8cc75
Кирилл Шубарин <kirill.shubarin@gmail.com>
2021-01-21 16:04:12 +0300

    restructured folder

--- booking/reservation/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Reservation
-
-
-class ReservationForm(forms.ModelForm):
-    class Meta:
-        model = Reservation
-        fields = ('room', 'datetime_from', 'datetime_to')
